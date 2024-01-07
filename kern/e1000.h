736b8c85ddf5a24f23d7464bbbafa23080e15e4a
rda <2055017970@qq.com>
2024-01-07 14:47:27 +0800

    define tx_desc and rx_desc

--- kern/e1000.h
+++ kern/e1000.h
@@ -8,6 +8,35 @@
 #define E1000_DEVICE_ID_82540EM             0x100E
 
 
+
+/* Transmit Descriptor(TDESC) Definition */
+struct tx_desc
+{
+    /* data */
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t sta;
+    uint8_t rsv;
+    uint8_t css;
+    uint16_t special;
+};
+
+
+/* Receive Descriptor(RDESC) Definition */
+struct rx_desc
+{
+    /* data */
+    uint64_t addr;
+    uint16_t length;
+    uint16_t checksum;
+    uint8_t status;
+    uint8_t errors;
+    uint16_t special;
+};
+
+
 int e1000_init();
 int pci_e1000_attach(struct pci_func *pcif);
 
