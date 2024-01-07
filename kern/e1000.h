3a4c2e1a791094a033a3d2ff596f7522887acfaf
rda <2055017970@qq.com>
2024-01-07 17:46:44 +0800

    fix bug in tx_desc

--- kern/e1000.h
+++ kern/e1000.h
@@ -8,8 +8,56 @@
 #define E1000_DEVICE_ID_82540EM             0x100E
 
 
+/** PCI Controller Register
+  * RW - register is both readable and writable
+  * RO - register is read only
+  * WO - register is write only
+  **/
+#define E1000_CTRL                          0x0000   /* Device Control - RW */
+#define E1000_STATUS                        0x0008   /* Device Status - RO */
 
-/* Transmit Descriptor(TDESC) Definition */
+ 
+#define E1000_RCTL                          0x0100   /* RX Control - RW */
+#define E1000_RDBAL                         0x2800   /* Receive Descriptor Base Low - RW */
+#define E1000_RDBAH                         0x2804   /* Receive Descriptor Base High - RW */
+#define E1000_RDLEN                         0x2808   /* Receive Descriptor Length - RW */
+#define E1000_RDH                           0x2810   /* RX Descriptor Head (1) - RW */
+#define E1000_RDT                           0x2818   /* RX Descriptor Tail (1) - RW */
+
+
+#define E1000_TCTL                          0x0400   /* TX Control - RW */
+#define E1000_TIPG                          0x0410   /* TX IPG - RW */
+#define E1000_TDBAL                         0x3800   /* Transmit Descriptor Base Low - RW */
+#define E1000_TDBAH                         0x3804   /* Transmit Descriptor Base High - RW */
+#define E1000_TDLEN                         0x3808   /* Transmit Descriptor Length - RW */
+#define E1000_TDH                           0x3810   /* TX Descriptor Head (1) - RW */
+#define E1000_TDT                           0x3818   /* TX Descriptor Tail (1) - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_EN                       0x00000002    /* enable tx */
+#define E1000_TCTL_PSP                      0x00000008    /* pad short packets */
+#define E1000_TCTL_CT                       0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD                     0x003ff000    /* collision distance */
+
+
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_CMD_EOP                   0x01000000 /* End of Packet */
+#define E1000_TXD_CMD_IFCS                  0x02000000 /* Insert FCS (Ethernet CRC) */
+#define E1000_TXD_CMD_IC                    0x04000000 /* Insert Checksum */
+#define E1000_TXD_CMD_RS                    0x08000000 /* Report Status */
+
+#define E1000_TXD_STAT_DD                   0x00000001 /* Descriptor Done */
+#define E1000_TXD_STAT_EC                   0x00000002 /* Excess Collisions */
+#define E1000_TXD_STAT_LC                   0x00000004 /* Late Collisions */
+#define E1000_TXD_STAT_TU                   0x00000008 /* Transmit underrun */
+
+#define RING_SIZE                           0x0020
+#define BUFFER_SIZE                         1518
+
+#define INDEX(OFFSET)                       ((OFFSET) >> 2)
+
+
+/* ransmit Descriptor(TDESC) Definition */
 struct tx_desc
 {
     /* data */
@@ -18,7 +66,6 @@ struct tx_desc
     uint8_t cso;
     uint8_t cmd;
     uint8_t sta;
-    uint8_t rsv;
     uint8_t css;
     uint16_t special;
 };
@@ -38,7 +85,10 @@ struct rx_desc
 
 
 int e1000_init();
+int e1000_transmit(void * pkt);
 int pci_e1000_attach(struct pci_func *pcif);
 
+
+int check_e1000_transmit();
 
 #endif  // SOL >= 6
