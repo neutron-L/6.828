53a2e3302ea4a34f9c186e0188ea9b42755b9891
rda <2055017970@qq.com>
2024-01-07 14:06:46 +0800

    exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -4,8 +4,15 @@
 
 // LAB 6: Your driver code here
 
+volatile uint32_t *e1000;
+
+
 int pci_e1000_attach(struct pci_func* pcif)
 {
     pci_func_enable(pcif);
+
+    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("status word: 0x%x\n", *(e1000 + 2));
+    
     return 0;
 }
--- kern/lapic.c
+++ kern/lapic.c
@@ -61,7 +61,6 @@ lapic_init(void)
 	// lapicaddr is the physical address of the LAPIC's 4K MMIO
 	// region.  Map it in to virtual memory so we can access it.
 	lapic = mmio_map_region(lapicaddr, 4096);
-    cprintf("lapic_init: lapic = 0x%x; lapicaddr = 0x%x\n", lapic, lapicaddr);
 	// Enable local APIC; set spurious interrupt vector.
 	lapicw(SVR, ENABLE | (IRQ_OFFSET + IRQ_SPURIOUS));
 
